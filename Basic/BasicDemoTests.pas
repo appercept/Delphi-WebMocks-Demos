9a1c5fb1566abc612c7d615529908d7d71aa5fd8
Richard Hatherall <richard@appercept.com>
2021-02-06 16:05:07 +0000

    Add Basic Demo project

---
+++ Basic/BasicDemoTests.pas
@@ -0,0 +1,56 @@
+﻿unit BasicDemoTests;
+
+interface
+
+uses
+  DUnitX.TestFramework,
+  System.Net.HttpClient,
+  WebMock;
+
+type
+  [TestFixture]
+  TBasicDemoTests = class
+  private
+    Client: THTTPClient;
+    WebMock: TWebMock;
+  public
+    [Setup]
+    procedure Setup;
+    [TearDown]
+    procedure TearDown;
+    [Test]
+    procedure Get_WhenStubbed_ReturnsOK;
+  end;
+
+implementation
+
+procedure TBasicDemoTests.Get_WhenStubbed_ReturnsOK;
+var
+  LResponse: IHTTPResponse;
+begin
+  // Arrange
+  WebMock.StubRequest('GET', '/');
+
+  // Act
+  LResponse := Client.Get(WebMock.URLFor('/'));
+
+  // Assert
+  Assert.AreEqual(200, LResponse.StatusCode);
+end;
+
+procedure TBasicDemoTests.Setup;
+begin
+  Client := THTTPClient.Create;
+  WebMock := TWebMock.Create;
+end;
+
+procedure TBasicDemoTests.TearDown;
+begin
+  WebMock.Free;
+  Client.Free;
+end;
+
+initialization
+  TDUnitX.RegisterTestFixture(TBasicDemoTests);
+
+end.
